ecea3895d20223076f38dabedd62254642941fa5
Ubuntu <ubuntu@ip-172-31-31-87.ec2.internal>
2020-07-27 21:55:31 +0000

    preds

--- test.py
+++ test.py
@@ -58,16 +58,26 @@ def predict(text):
     probabilities = F.softmax(prediction, dim=1)
     probabilities = probabilities.detach().cpu().numpy()
     # print(probabilities)
-    return probabilities
+    # print(np.argmax(probabilities))
+    # prediction = 1 if probabilities[0][0] >= 0.5 else 0
+    prediction = np.argmax(probabilities)
+    return prediction
 
+    # texts = ["Forest fire near La Ronge Sask. Canada",
+    #         "Summer is lovely"
+    #         ]
+    # for text in texts:
+    #     preds = predict(text)
 
-# raw_text = "some text"
-# preds = predict(raw_text)
-# print(preds)
+# test_df = pd.read_csv("test.csv")
+# X_new = test_df[['text']]
+# test_df['target'] = test_df.text.apply(predict)
+# test_df.to_csv("sub.csv")
+# # pd.DataFrame({'id':test_df.id, 'target':predict(X_new)}).set_index('id').to_csv('sub1.csv')
 
-# import pandas as pd
-# df = pd.read_csv("test.csv")
-# df['prediction'] = df.text.apply(predict)
-# df.to_csv("train_preds.csv")
+import pandas as pd
+df = pd.read_csv("test.csv", usecols=["id", "text"])
+df['target'] = df.text.apply(predict)
+df.to_csv("charcnn-submission.csv")
 
 
